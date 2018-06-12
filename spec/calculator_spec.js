79256dc05c6fcabc8ad967334290d2c7c576dd93
oliver_ochman <oliver.ochman@hotmail.com>
2018-06-12 11:22:05 +0200

    refactor calculator spec

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -5,7 +5,7 @@ describe("BMICalculator", function() {
     var person;
 
     beforeEach(function(){
-        person = new Person({weight: 90, height: 186});
+        person = new Person({weight: 80, height: 186});
         calculator = new BMICalculator();
     });
 
